781d14f4d848097ea01592c048762b5008d8c53f
ArneS1 <arnoold12@gmx.de>
2020-12-16 19:07:26 +0100

    Level 1, Testing Area, Pömpel

---
+++ Assets/Scenes/Game/Level 1/Scripts/poempelReact.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class poempelReact : MonoBehaviour
+{
+    public AudioSource audioSource;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("plunger")){
+            audioSource.Play();
+            //TODO: Wasser ablassen
+            //TODO: Level gewinnen
+        }
+    }
+}
